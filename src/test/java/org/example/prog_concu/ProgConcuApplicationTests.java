33dcbf4e13f3276bd96884b93e76b84e4b2fb6d4
salmain2022 <salmabougha@example.com>
2025-06-12 19:28:25 +0100

    Initialisation de projet

---
+++ src/test/java/org/example/prog_concu/ProgConcuApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.prog_concu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProgConcuApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
